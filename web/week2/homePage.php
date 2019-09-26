ab2c416351cc39b01f940e9a9f3efc34b7367539
Korbin Dansie <dan15013@byui.edu>
2019-09-26 12:33:30 -0600

    added personal home page

---
+++ web/week2/homePage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" type="text/css" href="../css/HomeButton.css" />
+  <link rel="icon" href="../img/KtechIcon.png">
+  <title>Korbin Dansie's Home Page</title>
+</head>
+
+<body>
+
+  <h1><a href="index.html" id="homeButton">CS 213 Assignments</a></h1>
+
+  <h2>By: Korbin Dansie</h2>
+  <p>Br. Bradshaw</p>
+  <p>Date: 1/15/2019</p>
+  <p style="font-family:"Georgia"">Assignment 02</p>
+  <hr />
+
+
+  <h1 style="text-align:center;">Welcome to Korbin Dansie's Home Page</h1>
+
+  <p style="font-family:"Times New Roman""><strong>About Me:</strong></p>
+  <ul>
+    <li>Name: Korbin Marshal Dansie</li>
+    <li>Major: Computer Science</li>
+    <li>From: Utah</li>
+    <li style="font-family:"Book Antiqua;"">Favorite Quote:
+      <blockquote cite="https://www.poets.org/poetsorg/poem/road-not-taken">
+        Yet knowing how way leads on to way, <br />
+        I doubted if I should ever come back.<br />
+        <small><em>by Robert Frost</em></small>
+      </blockquote>
+    </li>
+  </ul>
+
+  <p>Interests</p>
+  <ul>
+    <li>Guitar</li>
+    <li>Computer Games</li>
+    <li>Drawing</li>
+  </ul>
+
+  <p />
+  <figure>
+    <!--Should this link also be relitve?-->
+    <img src="https://gregolsen.com/wp-content/uploads/2014/08/Worlds-Without-End.jpg"
+    alt="Worlds Without End" width="420" height="276">
+    <figcaption style="font-size:small;">Words Without End by: Greg Olsen</figcaption>
+  </figure>
+</body>
+</html>
