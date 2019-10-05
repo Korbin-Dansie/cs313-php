f29a5fa647c425eba9409b0f6ff369c3337eb077
Korbin Dansie <dan15013@byui.edu>
2019-10-05 17:52:28 -0600

    added images

---
+++ web/week3/topNav.php
@@ -0,0 +1,44 @@
+<div class="topnav">
+  <nav>
+    <h1><a href="week3.php" id="shopHomeButton">Shadow-Steel Weapons</a></h1>
+    <div id="navagation">
+      <div id="Products">
+        <ul>
+          <li><a href="errorPage.php">Swords</a>
+            <ul>
+              <li><a href="errorPage.php">Short Sword</a></li>
+              <li><a href="errorPage.php">Long Sword</a></li>
+              <li><a href="week3.php">Sharp Sword</a></li>
+            </ul>
+          </li>
+          <li><a href="errorPage.php">Bows</a>
+            <ul>
+              <li><a href="errorPage.php">Short Bow</a></li>
+              <li><a href="errorPage.php">Long Bow</a></li>
+              <li><a href="errorPage.php">Flat Bow</a></li>
+            </ul>
+          </li>
+          <li><a href="errorPage.php">Staffs</a>
+            <ul>
+              <li><a href="errorPage.php">Short Staffs</a></li>
+              <li><a href="errorPage.php">Long Staffs</a></li>
+              <li><a href="errorPage.php">Round Staffs</a></li>
+            </ul>
+          </li>
+        </ul>
+        <div id="loginbar">
+          <input type="text" placeholder="Search..">
+          <div id="loginButton">
+            <a href="errorPage.html">
+              <img src="img/PersonIcon.png" alt="Login Icon">
+              Login</a>
+            </div>
+            <a href="shopingCart.php">
+              <img src="img/ShopingCart.png" alt="Shoping Cart Icon">
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </nav>
+  </div><!--End of top nav-->
---
+++ web/week3/week3.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['newIteams'])){
+  $_SESSION['newIteams'] = array();
+}
+
+if (isset($_POST["addToCart"])) {
+  array_push($_SESSION['newIteams'], $_POST);
+  header("Location: week3.php");
+  return;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>Test Page</title>
+  <link rel="icon" href="images/KtechIcon.png">
+  <link rel="stylesheet" type="text/css" href="assign04.css" />
+  <link rel="stylesheet" type="text/css" href="../css/HomeButton.css" />
+  <link rel="stylesheet" type="text/css" href="assign06.css">
+  <script type="text/javascript" src="assign06.js">
+  </script>
+  <script type="text/javascript">
+  function hello()
+  {
+    alert("hello");
+  }
+  </script>
+
+  <style media="screen">
+  #finePrint{
+    font-size: x-small;
+    font-family: monospace;
+  }
+
+  span{
+    font-size: large;
+    font-family: sans-serif;
+  }
+
+  .mainContect h1{
+    border-bottom: #9966ff solid 2px;
+  }
+
+  .mainContect{
+    min-height: 600px;
+  }
+
+  #navagation{
+    width: 900px;
+    display: inline-block;
+  }
+
+  .topnav nav ul{
+    float: left;
+    padding-left: 5px;
+    margin-top: 0px;
+  }
+
+  table img{
+    display: inline-block;
+    height: 100px;
+    width: 100px;
+  }
+</style>
+
+</head>
+
+<body onload="resetForm()">
+  <header>
+    <h1><a href="../index.php" id="homeButton">CS 313 Assignments</a></h1>
+    <h2>By: Korbin Dansie</h2>
+    <p>Assignment 3</p>
+    <hr />
+  </header>
+
+  <div id="body">
+    <?php require_once('topNav.php');?>
+
+      <div class="sideAdd">
+      </div>
+
+
+      <div class="mainContect">
+        <h1>Shopping Cart</h1>
+        <form method="post">
+          <input type="submit" name="addToCart" value="addToCart" readonly>
+          <input type="text" name="itemName" value="sword" readonly>
+          <input type="text" name="itemPrice" value="10" readonly><br>
+        </form>
+        <form method="post">
+          <input type="submit" name="addToCart" value="addToCart" readonly>
+          <input type="text" name="itemName" value="shield" readonly>
+          <input type="text" name="itemPrice" value="15" readonly><br>
+        </form>
+
+      </div><!--End of Main mainContect-->
+
+      <footer>
+        <h2>This is a made up page</h2>
+        <p>We do not sell weapons and everything else on this site
+          is made up informaiton.</p>
+          <p>Also since this is made for school many of the links are broken.</p>
+          Copyright 2019. All Rights Reserved
+          <p id="finePrint"><small>We make <strong>no promise</strong> what so ever. Everything up above is made up.
+            If you order a weapon from up it may <strong>never even show up.</strong></small></p>
+
+          </footer>
+
+        </div><!--End of body-->
+
+      </body>
+      </html>
