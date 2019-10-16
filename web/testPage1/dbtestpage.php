360b4183653c758d707d43e5e333d5a0be6080a0
Korbin Dansie <dan15013@byui.edu>
2019-10-16 10:32:10 -0600

    added html

---
+++ web/testPage1/dbtestpage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" type="text/css" href="../css/HomeButton.css" />
+  <link rel="icon" href="../img/KtechIcon.png">
+  <title>Korbin Dansie's</title>
+</head>
+
+<body>
+  <h1><a href="../index.php" id="homeButton">CS 313 Assignments</a></h1>
+  <h2>By: Korbin Dansie</h2>
+  <br />
+
+
+<?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+ ?>
+</body>
