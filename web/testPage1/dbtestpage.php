361f1c131ca1b2ae779dd999731cd3ccab83ac44
Korbin Dansie <dan15013@byui.edu>
2019-10-16 16:56:33 -0600

    db

--- web/testPage1/dbtestpage.php
+++ web/testPage1/dbtestpage.php
@@ -13,36 +13,37 @@
   <br />
 
 
-<?php
+  <?php
 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+    $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Got Here.";
-  $stmt = $db->query("select * from public.customers");
-  foreach ($stmt->fetch() as $row) {
-    // code...
-    echo "<br />";
-    print $row['userid']."-".$row['name']."<br/>";
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Got Here.";
+    $stmt = $db->query("select * from public.customers");
+    while ($row = $stmt->fetch()) {
+      // code...
+      echo "<br />";
+      echo $row['name']."<br />\n";
+    }
   }
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
 
- ?>
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+
+  ?>
 </body>
