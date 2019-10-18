90c77beb29bd6ecbc0792f1f0a586941a6929452
Korbin Dansie <dan15013@byui.edu>
2019-10-18 12:51:45 -0600

    test

--- web/testPage1/displayProducts.php
+++ web/testPage1/displayProducts.php
@@ -1,33 +1,35 @@
 <?php
 //Print the table in HTML
 function writeProductsTable($WHEREClause = '')
-try
 {
-  $dbUrl = getenv('DATABASE_URL');
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+    $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-  $statement = $db->query('SELECT * FROM products ');
-  $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-  print_r($results);
+    $statement = $db->query('SELECT * FROM products ');
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    print_r($results);
 
-}//End of Try
+  }//End of Try
 
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
 }
 
 ?>
