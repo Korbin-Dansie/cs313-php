24cc0bf79312b50174654b04ce942ac4e61c7525
Korbin Dansie <dan15013@byui.edu>
2019-10-18 12:49:36 -0600

    Added DisplayProducts

--- web/testPage1/dbtestpage.php
+++ web/testPage1/dbtestpage.php
@@ -8,41 +8,12 @@
 </head>
 
 <body>
-  <h1><a href="../index.php" id="homeButton">CS 313 Assignments</a></h1>
+  <h1><a href="../index.html" id="homeButton">CS 313 Assignments</a></h1>
   <h2>By: Korbin Dansie</h2>
   <br />
 
 
   <?php
-
-  try
-  {
-    $dbUrl = getenv('DATABASE_URL');
-
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-    $statement = $db->query('SELECT * FROM products');
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    print_r($results);
-
-  }//End of Try
-
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
-
+  include("displayProducts.php");
   ?>
 </body>
