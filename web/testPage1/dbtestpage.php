1c8a4847a6fdb1c3f91801a4c6444b7aa43ef993
Korbin Dansie <dan15013@byui.edu>
2019-10-18 14:20:29 -0600

    a8

--- web/testPage1/dbtestpage.php
+++ web/testPage1/dbtestpage.php
@@ -18,7 +18,6 @@
       <input type="Button" name="Submit" value="Submit" onclick="updateProducts()">
     </form>
 
-<<<<<<< HEAD
 
 
 
@@ -32,35 +31,8 @@
     var xhr = new XMLHttpRequest();
     xhr.onreadyState = function (){
       if (xhr.readyState == 4 && xhr.status == 200) {
-        //Do Stuff
-
         var divDom = document.getElementById("producTable");
         divDom.innerHTML = xhr.responseText;
-=======
-  <?php
-
-  try
-  {
-    $dbUrl = getenv('DATABASE_URL');
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $statement = $db->query('SELECT * FROM products');
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    print_r($results);
-
-  }//End of Try
->>>>>>> d9e3e93482bc9625f3cbefc211dbfab276f21b6c
-
       }
     }
     xhr.open("GET", "displayProducts.php");
