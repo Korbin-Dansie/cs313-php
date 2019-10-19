7688607b45bbd85b459ed30a9dceb5ab6db7daa5
Korbin Dansie <dan15013@byui.edu>
2019-10-19 15:01:15 -0600

    FETCH_NAMED

--- web/testPage1/displayProducts.php
+++ web/testPage1/displayProducts.php
@@ -71,7 +71,7 @@ try
   ';
 
   $dbquery = $db->query($statment . ' ' . $WHEREclause);
-  $results = $dbquery->fetchAll(PDO::FETCH_CLASS);
+  $results = $dbquery->fetchAll(PDO::FETCH_NAMED);
   echo "'";
   print_r($statment);
   echo "'";
