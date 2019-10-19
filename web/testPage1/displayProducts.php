b8e1c178092416090fe9d05d251cc1f2d85ea5dc
Korbin Dansie <dan15013@byui.edu>
2019-10-19 15:20:46 -0600

    t2

--- web/testPage1/displayProducts.php
+++ web/testPage1/displayProducts.php
@@ -71,7 +71,7 @@ try
   ';
 
   $dbquery = $db->query($statment . ' ' . $WHEREclause);
-  $results = $dbquery->fetchAll(PDO::FETCH_BOTH,PDO::FETCH_ASSOC);
+  $results = $dbquery->fetchAll(PDO::FETCH_BOTH,PDO::FETCH_CLASS);
   echo "'";
   print_r($statment);
   echo "'";
