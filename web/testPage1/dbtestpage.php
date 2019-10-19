cd1d8b26cb93dd4e35b2a36ab007e7ae88702b9d
Korbin Dansie <dan15013@byui.edu>
2019-10-18 18:30:24 -0600

    prms8

--- web/testPage1/dbtestpage.php
+++ web/testPage1/dbtestpage.php
@@ -7,34 +7,36 @@
   <title>Korbin Dansie's</title>
   <script type="text/javascript">
   function updateProducts(){
+
     var xhr = new XMLHttpRequest();
+
     xhr.onreadystatechange = function (){
       if (xhr.readyState == 4 && xhr.status == 200) {
         var divDom = document.getElementById("productTable");
         divDom.innerHTML = xhr.responseText;
       }
     }
-
-    //Prepare the Get String
-    var getString = "?";
-    var formLocation = document.getElementById("SearchForm");
-    var formElements = formLocation.getElementsByTagName("INPUT");
-    for (var i = 0, element; element = formElements[i++];) {
-      if (element.value !== ""){
-        getString += element.getAttribute("name") + "=" + element.value + "&";
-      }
-    }
-
-    //Trim last charactar of the string to prevent errors
-    getString = getString.substring(0, getString.length - 1);
-    window.history.replaceState(null, null, getString);
     //Get String is prepared
     //Pass in the current GET Paramaters
     var paramaters = "";
+    if(location.search == null){
+      //Prepare the Get String
+      var getString = "?";
+      var formLocation = document.getElementById("SearchForm");
+      var formElements = formLocation.getElementsByTagName("INPUT");
+      for (var i = 0, element; element = formElements[i++];) {
+        if (element.value !== ""){
+          getString += element.getAttribute("name") + "=" + element.value + "&";
+        }
+      }
+      //Trim last charactar of the string to prevent errors
+      getString = getString.substring(0, getString.length - 1);
+      window.history.replaceState(null, null, getString);
+    }
     if(location.search != null)
     {
       paramaters = location.search.toString();
-      }
+    }
     xhr.open("GET", "displayProducts.php" + paramaters);
     xhr.send(null);
   }
