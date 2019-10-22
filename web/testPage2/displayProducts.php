f40f08dd95b39836ac1d946550bb4dcaf6b54757
Korbin Dansie <dan15013@byui.edu>
2019-10-22 16:15:28 -0600

    pre4

--- web/testPage2/displayProducts.php
+++ web/testPage2/displayProducts.php
@@ -102,7 +102,7 @@ try
   $stmt->bindParam(1, $_GET['productsname']);
   $stmt->execute();
 
-  $results = $dbquery->fetchAll(PDO::FETCH_ASSOC);
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   //Create the tableRows
   $returnString .= "<table id='productTable'>";
   //Create table headers
