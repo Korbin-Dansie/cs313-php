110ad7b6a9837a687be91692891120ef3b903337
Korbin Dansie <dan15013@byui.edu>
2019-10-24 17:08:30 -0600

    css1

--- web/testPage2/displayProductsUI.php
+++ web/testPage2/displayProductsUI.php
@@ -6,14 +6,11 @@ $returnString = "";
 
  ?>
  <div>
-  <form method="get" action="" id="SearchForm" onreset="resetForm()">;
-    Name: <input type="text" name="ProductName">
-     <br/>
-     Price <input type="text" name="PriceLow"><input type="text" name="PriceHigh">
-     <br/>
-     Category <input type="text" name="Category">
-     <br/>
-     <input type="reset"  name="Reset" value="Clear">
-     <input type="button" name="Submit" value="Submit" onclick="updateProducts()">
+  <form method="get" action="" id="SearchForm" onreset="resetForm()">
+    <span>Name:</span><input type="text" name="ProductName">
+     <span>Price</span><input type="text" name="PriceLow"><input type="text" name="PriceHigh">
+     <span>Category</span><input type="text" name="Category">
+     <input id="ResetButton" type="reset"  name="Reset" value="Clear">
+     <input id="SubmitButton" type="button" name="Submit" value="Submit" onclick="updateProducts()">
    </form>
  </div>
