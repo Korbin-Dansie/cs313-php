3c378763a8798e932653b5b69aedfa10624dac61
Korbin Dansie <dan15013@byui.edu>
2019-10-25 15:30:47 -0600

    all

--- web/testPage1/displayProductsUI.php
+++ web/testPage1/displayProductsUI.php
@@ -36,7 +36,7 @@ try
   * [rarityname] => Common [productsname] => Steel_Sword [productsquantity] => 100
   * [productsprice] => 12
   ***********/
-  $statment = 'Select name from category';
+  $statment = 'Select * from category';
 
   $dbquery = $db->query($statment);
   $results = $dbquery->fetchAll(PDO::FETCH_ASSOC);
