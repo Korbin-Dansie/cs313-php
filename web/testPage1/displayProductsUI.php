0329356417b81c9af06ed8b070b90897842048be
Korbin Dansie <dan15013@byui.edu>
2019-10-25 15:21:48 -0600

    header?

--- web/testPage1/displayProductsUI.php
+++ web/testPage1/displayProductsUI.php
@@ -1,50 +1,52 @@
-<?php  ?>
- <div>
+<?php
+header("Content-Type: text/plain");
+ ?>
+<div>
   <form method="get" action="" id="SearchForm" onreset="resetForm()">
     <span>Name:</span><input type="search" name="ProductName">
-     <span>Price</span><input type="text" name="PriceLow" placeholder="Min"><input type="text" name="PriceHigh" placeholder="Max">
-     <span>Category</span>
-     <select id="CatagoryField" name="Category">
-       <option value="None">None</option>
-     </select>
+    <span>Price</span><input type="text" name="PriceLow" placeholder="Min"><input type="text" name="PriceHigh" placeholder="Max">
+    <span>Category</span>
+    <select id="CatagoryField" name="Category">
+      <option value="None">None</option>
+    </select>
 
-     <input id="ResetButton" type="reset"  name="Reset" value="Clear">
-     <input id="SubmitButton" type="button" name="Submit" value="Submit" onclick="updateProducts()">
-   </form>
-   <?php //Create a query to add all options to Catagory
-   echo "Try";
-   try
-   {
-     $dbUrl = getenv('DATABASE_URL');
+    <input id="ResetButton" type="reset"  name="Reset" value="Clear">
+    <input id="SubmitButton" type="button" name="Submit" value="Submit" onclick="updateProducts()">
+  </form>
+</div>
+<?php //Create a query to add all options to Catagory
+echo "Try";
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
 
-     $dbOpts = parse_url($dbUrl);
+  $dbOpts = parse_url($dbUrl);
 
-     $dbHost = $dbOpts["host"];
-     $dbPort = $dbOpts["port"];
-     $dbUser = $dbOpts["user"];
-     $dbPassword = $dbOpts["pass"];
-     $dbName = ltrim($dbOpts["path"],'/');
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-     /***********
-     * Returns an array with the following information
-     * [productsid] => 1      [categoryname] => Sword       [sub_categoryname] => Short_Sword
-     * [rarityname] => Common [productsname] => Steel_Sword [productsquantity] => 100
-     * [productsprice] => 12
-     ***********/
-     echo "Selects";
-     $statment = 'Select category.name from public.category;';
+  /***********
+  * Returns an array with the following information
+  * [productsid] => 1      [categoryname] => Sword       [sub_categoryname] => Short_Sword
+  * [rarityname] => Common [productsname] => Steel_Sword [productsquantity] => 100
+  * [productsprice] => 12
+  ***********/
+  echo "Selects";
+  $statment = 'Select category.name from public.category;';
 
-     $dbquery = $db->query($statment);
-     $results = $dbquery->fetchAll(PDO::FETCH_ASSOC);
-     echo "Here2";
-   catch (PDOException $ex)
-   {
-     echo 'Error!: ' . $ex->getMessage();
-     die();
-   }
-    ?>
- </div>
+  $dbquery = $db->query($statment);
+  $results = $dbquery->fetchAll(PDO::FETCH_ASSOC);
+  echo "Here2";
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+  ?>
