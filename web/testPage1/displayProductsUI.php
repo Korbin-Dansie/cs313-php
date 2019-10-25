212e24acc85f6522128305b984ca4caee8a7a841
Korbin Dansie <dan15013@byui.edu>
2019-10-25 15:22:22 -0600

    no html

--- web/testPage1/displayProductsUI.php
+++ web/testPage1/displayProductsUI.php
@@ -1,19 +1,7 @@
 <?php
 header("Content-Type: text/plain");
  ?>
-<div>
-  <form method="get" action="" id="SearchForm" onreset="resetForm()">
-    <span>Name:</span><input type="search" name="ProductName">
-    <span>Price</span><input type="text" name="PriceLow" placeholder="Min"><input type="text" name="PriceHigh" placeholder="Max">
-    <span>Category</span>
-    <select id="CatagoryField" name="Category">
-      <option value="None">None</option>
-    </select>
 
-    <input id="ResetButton" type="reset"  name="Reset" value="Clear">
-    <input id="SubmitButton" type="button" name="Submit" value="Submit" onclick="updateProducts()">
-  </form>
-</div>
 <?php //Create a query to add all options to Catagory
 echo "Try";
 try
