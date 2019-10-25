aed90ae4ed4221725bf1f4d9088628103db57c3d
Korbin Dansie <dan15013@byui.edu>
2019-10-25 15:11:02 -0600

    no php

--- web/testPage1/displayProductsUI.php
+++ web/testPage1/displayProductsUI.php
@@ -6,42 +6,7 @@
      <span>Category</span>
      <select id="CatagoryField" name="Category">
        <option value="None">None</option>
-       <?php //Create a query to add all options to Catagory
-       try
-       {
-         $dbUrl = getenv('DATABASE_URL');
 
-         $dbOpts = parse_url($dbUrl);
-
-         $dbHost = $dbOpts["host"];
-         $dbPort = $dbOpts["port"];
-         $dbUser = $dbOpts["user"];
-         $dbPassword = $dbOpts["pass"];
-         $dbName = ltrim($dbOpts["path"],'/');
-
-         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-         /***********
-         * Returns an array with the following information
-         * [productsid] => 1      [categoryname] => Sword       [sub_categoryname] => Short_Sword
-         * [rarityname] => Common [productsname] => Steel_Sword [productsquantity] => 100
-         * [productsprice] => 12
-         ***********/
-         $statment = 'Select name from category';
-
-         $dbquery = $db->query($statment);
-         $results = $dbquery->fetchAll(PDO::FETCH_ASSOC);
-
-         echo "$results";
-
-       catch (PDOException $ex)
-       {
-         echo 'Error!: ' . $ex->getMessage();
-         die();
-       }
-        ?>
      </select>
      <input id="ResetButton" type="reset"  name="Reset" value="Clear">
      <input id="SubmitButton" type="button" name="Submit" value="Submit" onclick="updateProducts()">
