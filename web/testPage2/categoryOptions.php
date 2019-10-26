b74a34155aaefc512993dacec9d6e4ba552c81c0
Korbin Dansie <dan15013@byui.edu>
2019-10-26 14:49:46 -0600

    categoryphp

---
+++ web/testPage2/categoryOptions.php
@@ -0,0 +1,41 @@
+<?php //Create a query to add all options to Catagory
+header("Content-Type: text/plain");
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  /***********
+  * Returns an array with the following information
+  * [productsid] => 1      [categoryname] => Sword       [sub_categoryname] => Short_Sword
+  * [rarityname] => Common [productsname] => Steel_Sword [productsquantity] => 100
+  * [productsprice] => 12
+  ***********/
+  $statment = 'Select name from category';
+
+  $dbquery = $db->query($statment);
+  $results = $dbquery->fetchAll(PDO::FETCH_ASSOC);
+  for ($i=0; $i < count($results); $i++) {
+    foreach ($results[$i] as $key => $value) {
+      $safeChars = htmlspecialchars($value);
+      echo "<option value=\"$safeChars\">$safeChars</option>";
+    }
+  }
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
--- web/testPage2/displayProductsUI.php
+++ web/testPage2/displayProductsUI.php
@@ -9,51 +9,13 @@
     <span>Category</span>
     <select id="CatagoryField" name="Category" onchange="displaySubProducts()">
       <option value="None">None</option>
-      <?php //Create a query to add all options to Catagory
-      try
-      {
-        $dbUrl = getenv('DATABASE_URL');
 
-        $dbOpts = parse_url($dbUrl);
-
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        /***********
-        * Returns an array with the following information
-        * [productsid] => 1      [categoryname] => Sword       [sub_categoryname] => Short_Sword
-        * [rarityname] => Common [productsname] => Steel_Sword [productsquantity] => 100
-        * [productsprice] => 12
-        ***********/
-        $statment = 'Select name from category';
-
-        $dbquery = $db->query($statment);
-        $results = $dbquery->fetchAll(PDO::FETCH_ASSOC);
-        for ($i=0; $i < count($results); $i++) {
-          foreach ($results[$i] as $key => $value) {
-            $safeChars = htmlspecialchars($value);
-            echo "<option value=\"$safeChars\">$safeChars</option>";
-          }
-        }
-      }
-      catch (PDOException $ex)
-      {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-      }
-      ?>
     </select>
     <br/>
     <span>Subcategory</span>
     <select id="SubCatagoryField" name="SubCategory">
       <option value="None">None</option>
+      <?php include("categoryOptions.php") ?>
     </select>
     <br/>
     <span>Rarity</span>
@@ -102,7 +64,6 @@
       ?>
     </select>
     <br/>
-    <input type="button" name="Hello" value="Hello" onclick="HelloWorld()">
     <input id="ResetButton" type="button"  name="Reset" value="Clear" onclick="resetForm()">
     <input id="SubmitButton" type="button" name="Submit" value="Submit" onclick="updateProducts()">
   </form>
