e561733bfed575e59814e4b29e9c241023683cd7
Korbin Dansie <dan15013@byui.edu>
2019-10-26 17:07:57 -0600

    testing php

--- web/testPage2/ProductAdd/prepareProduct.php
+++ web/testPage2/ProductAdd/prepareProduct.php
@@ -4,5 +4,18 @@ print_r($_GET);
 echo "<br>";
 echo "Post<br>";
 print_r($_POST);
+/* $_POST values
+* Array( [ProductName] => asdf [PriceLow] => 100 [Category] => Sword [SubCategory] => Short_Sword [Rarity] => Common )
+*
+*/
+echo "<br>";
+for ($i=0; $i < count($_POST); $i++) {
+  foreach ($_POST[$i] as $key => $value) {
+    echo "$key";
+    echo "---";
+    echo "$value";
+    echo "<br>";
+  }
+}
 
  ?>
