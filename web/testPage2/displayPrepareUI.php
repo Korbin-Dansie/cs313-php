746df733c169e4719a35b76a22bfd954898b6fae
Korbin Dansie <dan15013@byui.edu>
2019-10-26 15:02:33 -0600

    same as displayUI

---
+++ web/testPage2/displayPrepareUI.php
@@ -0,0 +1,29 @@
+<?php  ?>
+<div>
+  <form method="get" action="" id="SearchForm">
+    <span>Name</span><input type="search" name="ProductName">
+    <br/>
+    <span>Price</span><input type="text" name="PriceLow" placeholder="Min"><input type="text" name="PriceHigh" placeholder="Max">
+    <br/>
+
+    <span>Category</span>
+    <select id="CatagoryField" name="Category" onchange="displaySubProducts()">
+      <option value="None">None</option>
+      <?php include("categoryOptions.php") ?>
+    </select>
+    <br/>
+    <span>Subcategory</span>
+    <select id="SubCatagoryField" name="SubCategory">
+      <option value="None">None</option>
+    </select>
+    <br/>
+    <span>Rarity</span>
+    <select class="" name="Rarity">
+      <option value="None">None</option>
+      <?php include("rarityOptions.php") ?>
+    </select>
+    <br/>
+    <input id="ResetButton" type="button"  name="Reset" value="Clear" onclick="resetForm()">
+    <input id="SubmitButton" type="button" name="Submit" value="Submit" onclick="updateProducts()">
+  </form>
+</div>
