74d47c72603426e10aeb488c14bf6b7f966a197c
Korbin Dansie <dan15013@byui.edu>
2019-10-26 15:47:08 -0600

    2

--- web/testPage2/displayPrepareUI.php
+++ web/testPage2/displayPrepareUI.php
@@ -1,19 +1,19 @@
 <?php  ?>
 <div>
-  <form method="get" action="" id="SendForm">
+  <form method="get" action="" id="SearchForm2">
     <span>Name</span><input type="search" name="ProductName">
     <br/>
-    <span>Price</span><input type="text" name="PriceLow">
+    <span>Price</span><input type="text" name="PriceLow" placeholder="Min">
     <br/>
 
     <span>Category</span>
-    <select id="CatagoryField" name="Category" onchange="displaySubProducts()">
+    <select id="CatagoryField2" name="Category" onchange="displaySubProducts(&quot;CatagoryField2&quot;, &quot;SubCatagoryField2&quot;)">
       <option value="None">None</option>
       <?php include("categoryOptions.php") ?>
     </select>
     <br/>
     <span>Subcategory</span>
-    <select id="SubCatagoryField" name="SubCategory">
+    <select id="SubCatagoryField2" name="SubCategory">
       <option value="None">None</option>
     </select>
     <br/>
@@ -23,7 +23,8 @@
       <?php include("rarityOptions.php") ?>
     </select>
     <br/>
-    <input id="ResetButton" type="button"  name="Reset" value="Clear" onclick="resetForm(this.parentElement.id, productTable, CatagoryField, SubCatagoryField)">
-    <input id="SubmitButton" type="button" name="Submit" value="Submit" onclick="updateProducts(this.parentElement.id, null)">
+    <input id="ResetButton2" type="button"  name="Reset" value="Clear"
+    onclick="resetForm(this.parentElement.id, &quot;productTable2&quot;, &quot;CatagoryField2&quot;, &quot;SubCatagoryField2&quot;)">
+    <input id="SubmitButton2" type="button" name="Submit" value="Submit" onclick="updateProducts(this.parentElement.id, &quot;productTable2&quot;)">
   </form>
 </div>
