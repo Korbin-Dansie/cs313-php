2cd9bf36a3d77eee7862ab39fe378cee74b3f29d
Korbin Dansie <dan15013@byui.edu>
2019-10-26 21:37:23 -0600

    testing

--- web/testPage2/ProductSubtract/addToCartAjax.js
+++ web/testPage2/ProductSubtract/addToCartAjax.js
@@ -1,9 +1,4 @@
 function addToCart(datebaseRowInfo){
-
-  if(datebaseRowInfo == ""){
-    return;
-  }
-
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function (){
     if (xhr.readyState == 4 && xhr.status == 200) {
