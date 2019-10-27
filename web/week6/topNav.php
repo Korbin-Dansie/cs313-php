d952fc49dc113ccf41884528eb94214c4f3a2b44
Korbin Dansie <dan15013@byui.edu>
2019-10-26 23:18:08 -0600

    html

--- web/week6/topNav.php
+++ web/week6/topNav.php
@@ -6,9 +6,9 @@
         <ul>
           <li><a href="week6.php">Swords</a>
             <ul>
-              <li><a href="week6.php">Common Sword</a></li>
-              <li><a href="wee6.php">Uncommon Sword</a></li>
-              <li><a href="week6php">Rare Sword</a></li>
+              <li><a href="week6.php?Rarity=Common">Common Sword</a></li>
+              <li><a href="wee6.php?Rarity=Uncommon">Uncommon Sword</a></li>
+              <li><a href="week6php?Rarity=Rare">Rare Sword</a></li>
             </ul>
           </li>
           <li><a href="addProduct.php">Add Product</a>
