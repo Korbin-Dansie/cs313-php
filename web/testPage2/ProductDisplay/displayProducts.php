523b867aca56d0e0390a4e6e51dbe01d722c3b4e
Korbin Dansie <dan15013@byui.edu>
2019-10-26 21:07:28 -0600

    td added

--- web/testPage2/ProductDisplay/displayProducts.php
+++ web/testPage2/ProductDisplay/displayProducts.php
@@ -143,7 +143,7 @@ try{
       else if($key == "productsid"){
         //$returnString .= "<td value=\"$value\">".($i+1)."</td>";
         //Button to remove 1 product from the table
-        $button = "<button type=\"button\" value=\"$value\" onclick=\"addToCart($results[$i])\">" . ($i+1) . "</button>"
+        $button = "<td><button type=\"button\" value=\"$value\" onclick=\"addToCart($results[$i])\">" . ($i+1) . "</button></td>"
 
       }
       else {
