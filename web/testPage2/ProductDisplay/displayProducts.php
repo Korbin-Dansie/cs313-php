61789b63331c77e011b9abeec135b0819c4d5436
Korbin Dansie <dan15013@byui.edu>
2019-10-26 22:49:09 -0600

    before

--- web/testPage2/ProductDisplay/displayProducts.php
+++ web/testPage2/ProductDisplay/displayProducts.php
@@ -111,7 +111,7 @@ try{
   ON Sub_Category.categoryid = Category.id
   ';
 
-  $dbquery = $db->query($statment . " " . $WHEREclause . " Limit " . $limitNumber . " ORDER BY ProductsID ASC");
+  $dbquery = $db->query($statment . " " . $WHEREclause . " ORDER BY ProductsID ASC " . " Limit " . $limitNumber);
   $results = $dbquery->fetchAll(PDO::FETCH_ASSOC);
   //Create the tableRows
   $returnString .= "<table id='productTable'>";
