89bb8ed1674d786f4f743021e035cf8e422c399f
Korbin Dansie <dan15013@byui.edu>
2019-10-26 21:49:17 -0600

    console log

--- web/testPage2/ProductSubtract/addToCartAjax.js
+++ web/testPage2/ProductSubtract/addToCartAjax.js
@@ -14,6 +14,16 @@ function addToCart(datebaseRowInfo){
   alert(getString);
 
   //Trim last charactar of the string to prevent errors
+  console.log(location.href);      // https://developer.mozilla.org:8080/en-US/search?q=URL#search-results-close-container
+  console.log(location.protocol);  // https:
+  console.log(location.host);      // developer.mozilla.org:8080
+  console.log(location.hostname);  // developer.mozilla.org
+  console.log(location.port);      // 8080
+  console.log(location.pathname);  // /en-US/search
+  console.log(location.search);    // ?q=URL
+  console.log(location.hash);      // #search-results-close-container
+  console.log(location.origin);    // https://developer.mozilla.org:8080
+
   xhr.open("POST", "ProductSubtract/addToCart.php", true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhr.send(getString);
