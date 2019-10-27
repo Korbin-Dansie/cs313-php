68cd2ff4eeb8e07d34a88b9270f965ccb41f206c
Korbin Dansie <dan15013@byui.edu>
2019-10-26 21:10:50 -0600

    im confused

--- web/testPage2/ProductDisplay/displayProducts.php
+++ web/testPage2/ProductDisplay/displayProducts.php
@@ -143,7 +143,7 @@ try{
       else if($key == "productsid"){
         //$returnString .= "<td value=\"$value\">".($i+1)."</td>";
         //Button to remove 1 product from the table
-        $button = "<td><button value=\"$value\" onclick=\"addToCart($results[$i])\">" . ($i+1) . "</button></td>"
+        $button = "<td><button value=\"$value\" onclick=\"addToCart(" . $results[$i] .")\">" . ($i+1) . "</button></td>"
         $returnString .= $button;
       }
       else {
