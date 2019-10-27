9d5e352485c538d3c72325f1e92e46b72bcc06d0
Korbin Dansie <dan15013@byui.edu>
2019-10-26 21:58:18 -0600

    added php

--- web/testPage2/ProductSubtract/addToCart.php
+++ web/testPage2/ProductSubtract/addToCart.php
@@ -2,5 +2,24 @@
 header('Content-Type: text/plain; charset=utf-8');
 
 echo ($_POST['productid'] . " From php");
+if(is_numeric($_POST['productid']) == false){
+  return;
+}
 
+include("AllProductQuery.php");
+$inRange = AllProductQuery(" Where product.id = " . $_POST['productid']);
+if($inRange == ""){
+  echo "$inRange";
+  return;
+}
+
+
+if(!isset($_SESSION["shopping"]){
+  $_SESSION["shopping"] = array();
+  array_push($_SESSION["shopping"], $_POST['productid']);
+}else {
+  array_push($_SESSION["shopping"], $_POST['productid']);
+}
+
+return "Success";
  ?>
