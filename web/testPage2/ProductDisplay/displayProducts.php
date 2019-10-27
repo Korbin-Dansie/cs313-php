569206340bc9e1708723f3836c96cfd2429db68e
Korbin Dansie <dan15013@byui.edu>
2019-10-26 21:11:45 -0600

    just buttons

--- web/testPage2/ProductDisplay/displayProducts.php
+++ web/testPage2/ProductDisplay/displayProducts.php
@@ -142,8 +142,8 @@ try{
       }
       else if($key == "productsid"){
         //$returnString .= "<td value=\"$value\">".($i+1)."</td>";
-        //Button to remove 1 product from the table
-        $button = "<td><button value=\"$value\" onclick=\"addToCart(" . $results[$i] .")\">" . ($i+1) . "</button></td>"
+        //Button to remove 1 product from the table onclick=\"addToCart(" . $results[$i] .")\">"
+        $button = "<td><button value=\"$value\">"  . ($i+1) . "</button></td>"
         $returnString .= $button;
       }
       else {
