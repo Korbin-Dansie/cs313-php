6f4f368f593e3a2ebf92d747f556b4883ad039f0
Korbin Dansie <dan15013@byui.edu>
2019-10-26 18:34:00 -0600

    removed new added check

--- web/testPage2/ProductAdd/prepareProduct.php
+++ web/testPage2/ProductAdd/prepareProduct.php
@@ -34,4 +34,66 @@ isset($_POST['Category']) && isset($_POST['SubCategory']) && isset($_POST['Rarit
   echo "All variables are not set!";
   return;
 }
+
+include("../QueryOptions/CategoriesQuery.php");
+$dbInfoCategories = CategoriesQuery();
+
+
+$correctValues = true;
+//Check to make sure all variable have correct values
+if($_POST['ProductName'] == ""){
+  $correctValues = false;
+  array_push($returnStringArray, "Name Cannot Be Blank");
+
+}elseif (substr($_POST['ProductName'], 0, 5) == "@ERR:") {
+  $correctValues = false;
+  array_push($returnStringArray, "Name Cannot start with @ERR:");
+
+  // code...
+}elseif ( strpos($_POST['ProductName'], "'") === false) {
+  $correctValues = false;
+  array_push($returnStringArray, "Name Cannot contain a ' ");
+}
+
+if(!is_numeric($_POST['PriceList'])){
+  $correctValues = false;
+  array_push($returnStringArray, "Price need to be a number");
+}
+
+//Check if subcategorys are $correctValues
+//[Category] => Sword [SubCategory] => Short_Sword
+for ($i=0; $i <= count($dbInfoCategories); $i++) {
+  if($i == count($dbInfoCategories)){
+    $correctValues = false;
+    array_push($returnStringArray, "Category or SubCategory is incorrect");
+  }
+  else if($dbInfoCategories[$i]['CategoryName'] == $_POST['Category']){
+    if ($dbInfoCategories[$i]['Sub_CategoryName'] == $_POST['SubCategory']) {
+      //Correct value and subCategory
+      break;
+    }
+  }
+}
+
+
+//Check if rarity matches up
+include("../QueryOptions/RarityQuery.php");
+$dbInfoRarity = RarityQuery();
+for ($i=0; $i <= count($dbInfoRarity); $i++) {
+  if($i == count($dbInfoRarity)){
+    $correctValues = false;
+    array_push($returnStringArray, "Rarity is incorrect");
+  }
+  if($dbInfoRarity[$i]['name'] == $_POST['Rarity']){
+    //Correct value and subCategory
+    break;
+  }
+}
+
+if($correctValues == false){
+  echo '$correctValues<br> = ' . $correctValues;
+}
+
+print_r($returnStringArray);
+
 ?>
