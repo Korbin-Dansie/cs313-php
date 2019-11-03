9f99c22a38cd05524e0e43835a1404ee59074a22
Korbin Dansie <dan15013@byui.edu>
2019-11-02 21:56:44 -0600

    test

--- web/week7/RunningoutofTime/addmoney.php
+++ web/week7/RunningoutofTime/addmoney.php
@@ -3,16 +3,16 @@ session_start();
 //If we do not have a signed person return
 if(!isset($_SESSION['Username'])){
   header('Location: ../shopingCart.php');
-  return;
+  die();
 }
 
 //If nuber is not valid return
 if(!isset($_POST['addMoney'])) {
   header('Location: ../shopingCart.php');
-  return;
+  die();
 } else if(!is_numeric($_POST['addMoney'])) {
   header('Location: ../shopingCart.php');
-  return;
+  die();
 }
 
 //Add money to account
@@ -20,12 +20,13 @@ if(!isset($_POST['addMoney'])) {
 include('../QueryOptions/moneyQuery.php');
 $money = getMoneyQuery($_SESSION['Username']);
 if($money != null){
+  echo "Got Here";
   setMoneyQuery($_SESSION['Username'], ($_POST['addMoney'] + $money));
 }
 else {
   setMoneyQuery($_SESSION['Username'], $_POST['addMoney']);
 }
 header('Location: ../shopingCart.php');
-
+die();
 
 ?>
