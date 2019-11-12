324c38578475821cca64186c9c624d3df24b1190
Korbin Dansie <dan15013@byui.edu>
2019-11-12 11:21:45 -0700

    packages

---
+++ web/index.js
@@ -0,0 +1,48 @@
+/************************************************************
+* By: Korbin Dansie
+* Port number : 8888
+* Home page : http://localhost:8888/home
+  - Async Reads templatefile.html
+  - If file not detected just print Hello World
+************************************************************/
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function onRequest (req, res) {
+  console.log("Received a request for:" + req.url);
+  if(req.url == "/home"){
+    fs.readFile('templatefile.html', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err.name + ': ' + err.message);
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h1>Hello World</h1>");
+        res.end();
+        return;
+      }
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (req.url == "/getData") {
+    var data = {
+      name: "korbin Dansie",
+      class: "cs313"
+    };
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(JSON.stringify(data));
+    res.end();
+  }
+  else {
+    res.writeHead(404, {"Content-Type": "text/html"});
+    res.write("Page Not Found");
+    res.end();
+  }
+  console.log("End onRequest");
+}
+
+var server = http.createServer(onRequest);
+var port = 8888;
+server.listen(port);
+console.log("Listing for port:" + port);
